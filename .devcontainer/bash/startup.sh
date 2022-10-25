370060baaabb8408fca93d6d0116205f08236302
modamod <modhaffer.rahmani@gmail.com>
2022-10-25 08:15:09 +0100

    Added db volume and docker network to properly target containers

--- .devcontainer/bash/startup.sh
+++ .devcontainer/bash/startup.sh
@@ -13,17 +13,17 @@
 
 
 restore_database() {
-    mysql -u $1 -p$2 -h $3 -P $4  $5 < $6
+    mysql -u$1 -p$2 -h$3 -P$4  $5 < $6
 }
 
 
-: ${DB_HOST="mariabdb"}
+: ${DB_HOST="mariabdb.codespaces"}
 : ${DB_PORT="3306"}
 : ${OUTPUT="/workspaces/snipe-it/.gp/data/dump.sql"}
 mysqluserpw="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c16; echo)"
 wait-port $DB_HOST:$DB_PORT
-mysql -u root -h$DB_HOST -p$MYSQL_ROOT_PASSWORD -P$DB_PORT -e  "CREATE DATABASE IF NOT EXISTS snipeit;GRANT ALL PRIVILEGES ON snipeit.* TO 'snipeit'@'mariadb'; FLUSH PRIVILEGES;"
-if [[ ! $(mysql -u root -h$DB_HOST -p$MYSQL_ROOT_PASSWORD -P$DB_PORT -e "SELECT 1 FROM users LIMIT 1;" snipeit) ]]; then
+mysql -uroot -h$DB_HOST -p$MYSQL_ROOT_PASSWORD -P$DB_PORT -e  "CREATE DATABASE IF NOT EXISTS snipeit;GRANT ALL PRIVILEGES ON snipeit.* TO 'snipeit'@'mariadb'; FLUSH PRIVILEGES;"
+if [[ ! $(mysql -uroot -h$DB_HOST -p$MYSQL_ROOT_PASSWORD -P$DB_PORT -e "SELECT 1 FROM users LIMIT 1;" snipeit) ]]; then
     echo "RESTORING"
     restore_database "snipeit" $DB_PASSWORD "$DB_HOST" "$DB_PORT" "snipeit" $OUTPUT
 fi
